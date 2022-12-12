a34b0b8570dd75f5892e8d1eb2d805ca5cef3b6f
ippsav <mehdiboujid@gmail.com>
2022-12-12 19:34:02 +0000

    debug frontend api url

--- lib/api.ts
+++ lib/api.ts
@@ -34,7 +34,7 @@ export type LoginUserInput = {
   password: string
 };
 
-const API_URL = process.env.NODE_ENV === "production"
+export const API_URL = process.env.NODE_ENV === "production"
   ? process.env.NEXT_PUBLIC_API_URL : "http://localhost:8000"
 
 
